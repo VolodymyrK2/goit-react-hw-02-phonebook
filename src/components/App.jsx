95e5d962a1430e2b313cc60134ca6549fbd74be7
VolodymyrK2 <tema.rivne@gmail.com>
2023-05-07 18:31:51 +0300

    fixed duplication

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,22 +7,23 @@ import ContactList from './ContactsList/ContactsList'
 
 class App extends Component {
   state = {
-  contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
-  filter: ''
+    contacts: [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },],
+    filter: ''
   }
   changeContacts = (name, number) => {
-    this.state.contacts.find(contact => 
-      contact.name.toLowerCase() === name.toLowerCase())
-      && alert(`${ name } is already in contacts`)
-    
-    const newContact = { 'id': nanoid(), 'name': name, 'number':number }
-    this.setState((prevState) => ({
-    contacts: [newContact, ...prevState.contacts],
-    }))
-    }  
+     if (this.state.contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())){
+      alert(`${name} is already in contacts`)
+    } else {
+      const newContact = { 'id': nanoid(), 'name': name, 'number': number }
+      this.setState((prevState) => ({
+        contacts: [newContact, ...prevState.contacts],
+      }))
+    }
+  }
+
   changeFilter = ({target:{name, value}}) => {
     this.setState({
       [name]:value,
